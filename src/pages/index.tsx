16f35bff8462f4417428036f6e6769802d47633e
Wang Jiefan <wng.jiefan@gmail.com>
2023-03-08 09:21:07 +0800

    tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import type { HeadFC, PageProps } from "gatsby";
-import React, { FC } from "react";
-import FlexCards from "../components/FlexCards";
-import "../styles/index.sass";
+import type { HeadFC, PageProps } from "gatsby"
+import React, { FC } from "react"
+import FlexCards from "../components/FlexCards"
+import "../styles/index.sass"
 
 const IndexPage: FC<PageProps> = () => (
-  <div style={{width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+  <div className="flex w-screen h-100vh justify-center items-center">
     <FlexCards/>
   </div>
 );
 
-export default IndexPage;
+export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Home Page</title>
